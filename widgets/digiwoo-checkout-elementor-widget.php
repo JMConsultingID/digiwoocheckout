5441c006b503381aa4d1575834d18bf19a1d54df
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2023-10-26 16:02:47 +0800

    Update digiwoo-checkout-elementor-widget.php

--- widgets/digiwoo-checkout-elementor-widget.php
+++ widgets/digiwoo-checkout-elementor-widget.php
@@ -55,6 +55,16 @@ class Elementor_Digiwoo_Checkout_Elementor_Widget extends \Elementor\Widget_Base
             ]
         );
 
+        $this->add_control(
+            'add_on_product_category',
+            [
+                'label' => __( 'Select Add-On Product Category', 'plugin-name' ),
+                'type' => \Elementor\Controls_Manager::SELECT2,
+                'options' => $this->get_product_categories_dropdown(),
+                'label_block' => true,
+            ]
+        );
+
         $this->end_controls_section();
     }
 
@@ -65,6 +75,23 @@ class Elementor_Digiwoo_Checkout_Elementor_Widget extends \Elementor\Widget_Base
             $dropdown[$category->term_id] = $category->name;
         }
         return $dropdown;
+    }
+
+    private function get_products_by_category($category_id) {
+        $args = array(
+            'post_type' => 'product',
+            'posts_per_page' => -1,
+            'tax_query' => array(
+                array(
+                    'taxonomy' => 'product_cat',
+                    'field'    => 'id',
+                    'terms'    => $category_id,
+                    'operator' => 'IN',
+                ),
+            ),
+        );
+        $query = new WP_Query($args);
+        return $query->posts;
     }
 
     protected function render() {
@@ -79,5 +106,16 @@ class Elementor_Digiwoo_Checkout_Elementor_Widget extends \Elementor\Widget_Base
             }
             echo '</ul>';
         }
+
+        // Display products from the Add-On category
+        $add_on_category_id = $settings['add_on_product_category'];
+        $products = $this->get_products_by_category($add_on_category_id);
+
+        echo '<h2>Add-On Products:</h2>';
+        echo '<ul class="digiwoo-add-on-products">';
+        foreach ($products as $product) {
+            echo '<li>' . esc_html($product->get_name()) . '</li>';
+        }
+        echo '</ul>';
     }
 }
