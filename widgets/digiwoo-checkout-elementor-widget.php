ec0cf27fb28b5b6dc53a4142cff1bdfaa541b70a
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2023-10-27 00:20:57 +0800

    Update digiwoo-checkout-elementor-widget.php

--- widgets/digiwoo-checkout-elementor-widget.php
+++ widgets/digiwoo-checkout-elementor-widget.php
@@ -104,7 +104,7 @@ class Elementor_Digiwoo_Checkout_Elementor_Widget extends \Elementor\Widget_Base
             [
                 'label' => __('Hide Rule Based on Category', 'text-domain'),
                 'type' => \Elementor\Controls_Manager::SELECT,
-                'options' => array_merge(['none' => 'None'], $this->get_product_categories_dropdown()),  // Combining None option with categories
+                'options' => array_merge(['none' => 'None'], $this->get_product_category_options()),  // Combining None option with categories
                 'default' => 'none',
             ]
         );
@@ -215,6 +215,16 @@ class Elementor_Digiwoo_Checkout_Elementor_Widget extends \Elementor\Widget_Base
         $options = [];
         foreach ($products as $product) {
             $options[$product->ID] = $product->post_title;
+        }
+        return $options;
+    }
+
+    // Sample function to get product category options
+    protected function get_product_category_options() {
+        $categories = get_terms(['taxonomy' => 'product_cat']);
+        $options = [];
+        foreach ($categories as $category) {
+            $options[$category->term_id] = $category->name;
         }
         return $options;
     }
