3f941bd8a3064e87b95e750ea64edf96abec3c2d
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2023-10-27 00:15:12 +0800

    Update digiwoocheckout.js

--- assets/js/digiwoocheckout.js
+++ assets/js/digiwoocheckout.js
@@ -1,26 +1,20 @@
 (function( $ ) {
 	'use strict';
-	document.addEventListener('DOMContentLoaded', function() {
-	    const categoryRadios = document.querySelectorAll('.digiwoo-selected-product-categories input[type="radio"]');
-	    const addonProducts = document.querySelectorAll('.digiwoo-add-on-products .addon-product');
+	jQuery(document).ready(function($) {
+	    const $categoryRadios = $('.digiwoo-selected-product-categories input[type="radio"]');
+	    const $addonProducts = $('.digiwoo-add-on-products .addon-product');
 
-	    categoryRadios.forEach(radio => {
-	        radio.addEventListener('change', function() {
-	            const selectedCategory = this.value;
-	            
-	            addonProducts.forEach(product => {
-	                const hideRule = product.getAttribute('data-hide-rule');
-	                if (hideRule === selectedCategory) {
-	                    product.style.display = 'none';
-	                } else {
-	                    product.style.display = 'block';
-	                }
-	            });
+	    $categoryRadios.on('change', function() {
+	        const selectedCategory = $(this).val();
+	        
+	        $addonProducts.each(function() {
+	            const hideRule = $(this).data('hide-rule');
+	            if (hideRule === selectedCategory) {
+	                $(this).hide();
+	            } else {
+	                $(this).show();
+	            }
 	        });
 	    });
 	});
-
-
-
-
 })( jQuery );
