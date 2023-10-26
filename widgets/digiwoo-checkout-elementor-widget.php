1861eb7d534b7bc126d623ef51ec14f521e75558
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2023-10-26 15:56:26 +0800

    Update digiwoo-checkout-elementor-widget.php

--- widgets/digiwoo-checkout-elementor-widget.php
+++ widgets/digiwoo-checkout-elementor-widget.php
@@ -32,24 +32,6 @@ class Elementor_Digiwoo_Checkout_Elementor_Widget extends \Elementor\Widget_Base
             ]
         );
 
-        // Add product selection control
-        $products = get_posts(['post_type' => 'product', 'numberposts' => -1]);
-        $product_options = [];
-        foreach ($products as $product) {
-            $product_options[$product->ID] = $product->post_title;
-        }
-
-        $this->add_control(
-            'selected_product',
-            [
-                'label' => __('Select Product', 'digiwoocheckout'),
-                'type' => \Elementor\Controls_Manager::SELECT,
-                'options' => $product_options,
-	            'default' => 'internal',
-                'default' => ''
-            ]
-        );
-
         $repeater = new \Elementor\Repeater();
 
         // Add a dropdown to select WooCommerce product categories in each repeater item
@@ -86,23 +68,7 @@ class Elementor_Digiwoo_Checkout_Elementor_Widget extends \Elementor\Widget_Base
     }
 
     protected function render() {
-        $settings = $this->get_settings_for_display();?>
-
-        <section id="digiwoo-checkout-section" class="digiwoo-checkout-section my-3" style="margin-top:50px;">
-        <div class="row">
-    		<?php
-    		$product = wc_get_product($settings['selected_product']);
-			if ($product) {
-			    echo $product->get_image()."<br/>"; // Displays the product image
-			    echo $product->get_name()."<br/>"; // Product title
-			    echo $product->get_price_html()."<br/>"; // Price
-			}
-    		?>
-    	</div>
-    	</section>
-        <?php
-
-
+        $settings = $this->get_settings_for_display();
         if (!empty($settings['product_categories_list'])) {
             echo '<ul class="digiwoo-selected-product-categories">';
             foreach ($settings['product_categories_list'] as $item) {
