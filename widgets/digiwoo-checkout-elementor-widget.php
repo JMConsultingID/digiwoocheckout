17a24dfd654f3e715378ec630ebaac2518f56908
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2023-11-01 15:42:08 +0800

    Update digiwoo-checkout-elementor-widget.php

--- widgets/digiwoo-checkout-elementor-widget.php
+++ widgets/digiwoo-checkout-elementor-widget.php
@@ -184,7 +184,7 @@ class Elementor_Digiwoo_Checkout_Elementor_Widget extends \Elementor\Widget_Base
 
                     echo '<div class="addon-product" data-hide-rule="' . esc_attr($addon['rule_hide_on_category']) . '">';
                     echo '<label>';
-                    echo '<input type="checkbox" name="addon_product" value="' . esc_attr($product->get_id()) . '" data-percentage="' . esc_attr($percentage) . '">'. esc_html($product->get_name());
+                    echo '<input type="checkbox" name="addon_product[]" value="' . esc_attr($product->get_id()) . '" data-percentage="' . esc_attr($percentage) . '">'. esc_html($product->get_name());
                     echo '</label>';
                     echo '</div>';
                 }
